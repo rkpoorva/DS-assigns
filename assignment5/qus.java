6376b7e570e4e4fbe0114f5c5cea0cb7a9a3870c
rkpoorva <42445109+rkpoorva@users.noreply.github.com>
2018-10-18 22:44:18 +0530

    Add files via upload

---
+++ assignment5/qus.java
@@ -0,0 +1,198 @@
+import java.util.*; 
+
+class node 
+
+{
+  
+int data;
+  
+node next;
+  
+node(int d)
+  
+{data = d;} 
+
+}
+
+class Stack
+
+{
+  
+node head;
+  
+Scanner sc = new Scanner(System.in);
+  
+node push(int data)
+  
+{
+    
+node new1 = new node(data);
+    
+if(head == null)
+    
+{
+      head = new1;
+    }
+   
+else
+
+{
+
+new1.next = head;
+      
+head = new1;
+    
+}
+    
+return head;   
+  
+}
+  
+int pop()
+  
+{
+    
+if(head == null)
+    
+{
+      
+return 0;
+    
+}
+    
+else
+    
+{
+      
+int t = head.data;
+      
+head = head.next;
+      
+return t;
+    
+}
+  
+}
+  
+boolean isEmpty()
+  
+{
+     
+if(head == null)
+    
+{
+       
+return true;
+     
+}
+else
+     
+{
+        return false;
+     }
+  
+}
+
+}
+
+class Queue  
+
+{  
+    
+Stack s1 = new Stack();  
+    
+Stack s2 = new Stack();  
+  
+    
+void enQueue(int x)  
+    
+{  
+        
+while (!s1.isEmpty()) 
+        
+{  
+            
+s2.push(s1.pop());  
+        
+}   
+        
+s1.push(x);  
+        
+while (!s2.isEmpty())  
+        
+{  
+            
+s1.push(s2.pop());   
+        
+}  
+    
+}  
+    
+int deQueue()  
+    
+{  
+        
+if (s1.isEmpty())  
+        
+{  
+            
+System.out.println("Queue is Empty");    
+        
+}  
+
+        
+int x = s1.pop();  
+        
+return x;  
+    
+} 
+    
+public void traverse(int n)
+{
+
+        
+while(n > 0)
+{
+            
+int d = deQueue();
+           
+ System.out.print(d+" ");
+            
+n--;
+        
+}
+    
+} 
+
+}
+  
+public class QueueUsingStack
+{
+         
+        
+public static void main(String[] args)  
+        
+{  
+            
+Queue q = new Queue();  
+            
+Scanner sc = new Scanner(System.in);
+            
+System.out.println("Enter the size of queue");
+            
+int n = sc.nextInt();
+            
+for(int i = 0; i < n; i++)
+{
+                
+int d = sc.nextInt();
+                
+q.enQueue(d);
+            
+}  
+            
+q.traverse(n);
+        
+}  
+
+} 
---
+++ assignment5/suq.java
@@ -0,0 +1,229 @@
+import java.util.*; 
+
+class node
+
+{
+  
+int data;
+  
+node next;
+  
+node(int d)
+  
+{
+    
+data = d;
+    
+next = null; 
+  
+}
+
+}
+
+class Queue
+
+{
+  
+node head;
+  
+void add(int d)
+  
+{
+    
+node new1 = new node(d);
+    
+if(head == null)
+    
+{
+      head = new1; 
+    }
+    
+else
+    
+{
+      
+node t = head;
+      
+while(t.next != null)
+      
+{
+        t = t.next;
+      }
+      
+t.next = new1;
+    
+}
+  
+} 
+ 
+  
+boolean isEmpty()
+  
+{
+     return head == null;
+  }
+  
+int peek()
+  
+{
+    int x = head.data;
+    return x;
+  }
+  
+void remove()
+  
+{
+    
+int x = 0;
+    
+if(head == null)
+    
+{
+      System.out.println("underflow");
+    }
+    
+else
+    
+{
+     //x = head.data;
+      head = head.next;
+    }
+    
+//return x;
+  
+}
+  
+void display()
+  
+{
+     
+node t = head; 
+     
+while(t!=null)
+     
+{
+
+System.out.print(t.data+"->");
+         
+t = t.next;
+     
+}
+     
+System.out.println();
+  
+}
+
+}
+
+
+class Stack  
+
+{  
+     
+Queue q1 = new Queue();
+     
+Queue q2 = new Queue();   
+  
+    
+void push(int x)  
+    
+{  
+        
+q2.add(x);  
+  
+        
+while (!q1.isEmpty())  
+        
+{  
+            
+q2.add(q1.peek());  
+            
+q1.remove();  
+        
+}  
+
+        
+Queue tempq = q1;  
+        
+q1 = q2;  
+        
+q2 = tempq;  
+    
+}  
+  
+    
+void pop()
+{  
+
+        
+if (q1.isEmpty())  
+            
+return ;  
+        
+q1.remove();  
+    
+}  
+  
+    
+int top()  
+    
+{  
+        
+if (q1.isEmpty())  
+            
+return -1;  
+        
+return q1.peek();  
+    
+}  
+    
+void traverse(int n)
+{
+       
+while(n > 0)
+{
+        
+int d = top();
+        
+System.out.print(d+" ");
+        
+pop();
+        
+n--;
+       
+}
+
+}   
+
+}
+
+public class StackUsingQueue
+{
+
+    
+public static void main(String[] args)  
+    
+{  
+        
+System.out.println("Enter the size of stack");
+        
+Scanner sc = new Scanner(System.in);
+        
+int n = sc.nextInt();
+        
+Stack s = new Stack();  
+        
+for(int i = 0; i < n; i++)
+{
+            
+int d = sc.nextInt();
+            
+s.push(d);  
+        
+}
+        
+s.traverse(n);
+    
+} 
+
+} 
